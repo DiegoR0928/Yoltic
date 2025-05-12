d8b75bcc383d58c3828c802b7f15e0284f2ec0fb
Diego Gomez <diego.gcarlos@gmail.com>
2025-05-11 20:16:48 -0600

    Tres camaras funcionando

--- front_web/gstreamer/cliente.sh
+++ front_web/gstreamer/cliente.sh
@@ -1,16 +1,22 @@
 gst-launch-1.0 -v \
-  rtspsrc location=rtsp://192.168.1.79:8554/cam1 latency=100 ! \
-  decodebin ! videoconvert ! jpegenc ! \
+  rtspsrc location=rtsp://192.168.1.79:8554/cam1 latency=120 ! \
+  decodebin ! videoconvert ! \
+  videorate ! video/x-raw,framerate=10/1 ! videoscale ! video/x-raw,width=240,height=120 ! \
+  jpegenc ! \
   udpsink host=127.0.0.1 port=5000 &
 
 gst-launch-1.0 -v \
-  rtspsrc location=rtsp://192.168.1.79:8554/cam2 latency=100 ! \
-  decodebin ! videoconvert ! jpegenc ! \
+  rtspsrc location=rtsp://192.168.1.79:8554/cam2 latency=50 ! \
+  decodebin ! videoconvert ! \
+  videorate ! video/x-raw,framerate=30/1 ! videoscale ! video/x-raw,width=240,height=120 ! \
+  jpegenc ! \
   udpsink host=127.0.0.1 port=5001 &
 
 gst-launch-1.0 -v \
-  rtspsrc location=rtsp://192.168.1.79:8554/cam3 latency=100 ! \
-  decodebin ! videoconvert ! jpegenc ! \
-  udpsink host=127.0.0.1 port=5002 &
+  rtspsrc location=rtsp://192.168.1.79:8554/cam3 latency=120 ! \
+  decodebin ! videoconvert ! \
+  videorate ! video/x-raw,framerate=10/1 ! videoscale ! video/x-raw,width=240,height=120 ! \
+  jpegenc ! \
+  udpsink host=127.0.0.1 port=5002
 
 wait
